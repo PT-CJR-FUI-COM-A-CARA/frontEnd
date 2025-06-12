17d9a5a23516238dc00d77b217b41f9ea1642271
heyttor augusto <232002996@aluno.unb.br>
2025-06-12 01:15:18 -0300

    navebar icones

--- app/components/navbar/NavBar.tsx
+++ app/components/navbar/NavBar.tsx
@@ -14,6 +14,40 @@ export default function NavBar(){
                     </button>
                 </div>    
                 <ul className="flex space-x-4"> 
+                    <li> 
+                       <button
+                    arial-label = "mais"
+                    className = " p-1 rounded-full hover:scale-110 trasition duration-300 cursor-pointer"
+                    > <img src="/icones-nav/Mais_Icon.png" alt = "Mais" className = "h-8" />
+                    </button>  
+                          </li>
+
+                    <li>   
+                     <button
+                    arial-label = "notificações"
+                    className = " p-1 rounded-full hover:scale-110 trasition duration-300 cursor-pointer"
+                    > <img src="/icones-nav/Noti_Icon.png" alt = "notificações" className = "h-8" />
+                    </button>
+
+                    </li>
+
+                    <li>   
+                     <button
+                    arial-label = "Icon"
+                    className = " p-1 rounded-full hover:scale-110 trasition duration-300 cursor-pointer"
+                    > <img src="/icones-nav/PH_Icon.png" alt = "Icon" className = "h-8" />
+                    </button>
+
+                    </li>
+
+                    <li>   
+                          <button
+                    arial-label = "Sair"
+                    className = " p-1 rounded-full hover:scale-110 trasition duration-300 cursor-pointer"
+                    > <img src="/icones-nav/Saida_Icon.png" alt = "saida" className = "h-8" />
+                    </button>
+
+                    </li>
                 </ul>
             </div>
         </nav>
