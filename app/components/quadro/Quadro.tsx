c569b70cf2fb1d20ab65c0fd11eae11febefbbba
Joao Victor Felix Moreira <231037709@aluno.unb.br>
2025-06-17 03:33:58 -0300

    :lipstick:style: Alterando fonte do nome dos professores

--- app/components/quadro/Quadro.tsx
+++ app/components/quadro/Quadro.tsx
@@ -16,7 +16,7 @@ const ProfQuadro: React.FC<ProfProps> = ({ name, area, Fotopsrc }) => {
                 alt={name}
                 className="w-32 h-32 sm:w-32 sm:h-32 md:w-36 md:h-36 lg:w-40 lg:h-40 mx-auto mt-2 object-cover rounded-xl"
             />
-            <p className="mt-3 text-center text-sm sm:text-base font-semibold text-black">{name}</p>
+            <p className="mt-3 text-center text-sm sm:text-base font-medium text-black">{name}</p>
             <p className="mt-1 text-center text-xs sm:text-sm text-gray-600">{area}</p>
         </div>
     );
