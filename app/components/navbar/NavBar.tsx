9e90400ba9890ce7172c8ae7fad1e343644cf4af
jsalless <sanches.johnnatands@gmail.com>
2025-06-22 20:27:21 -0300

    Salvando alterações no NavBar antes de mudar de branch

--- app/components/navbar/NavBar.tsx
+++ app/components/navbar/NavBar.tsx
@@ -1,7 +1,7 @@
 'use client'
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
-import Botão from '../botao_branco/Botão';
+import Botão from '../botao_branco/Botao_branco';
 import { jwtDecode } from 'jwt-decode';
 import { getOneUser } from '@/app/utils/api'; // ajuste se necessário
 
